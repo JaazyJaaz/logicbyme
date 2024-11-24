888383cf5c15fced4efa2497a0a12922f89c686e
Jessica <159528808+JaazyJaaz@users.noreply.github.com>
2024-11-23 19:30:01 -0700

    change consts to define

--- src/_arduino/range-game/range_game.ino
+++ src/_arduino/range-game/range_game.ino
@@ -1,12 +1,13 @@
 #include <Stepper.h>
 #include <SoftwareSerial.h>
 
+#define upperBtn_pin 6
+@define upperBtnLed_pin 8
+#define lowerBtn_pin 13
+#define lowerBtnLed_pin 7
+
 const int stepsPerRevolution = 2038;
 Stepper rangeFinder = Stepper(stepsPerRevolution, 10, 12, 11, 9);
-const int upperBtn_pin = 6;
-const int upperBtnLed_pin = 8;
-const int lowerBtn_pin = 13;
-const int lowerBtnLed_pin = 7;
 
 void setup() {
   pinMode(upperBtn_pin, INPUT);
